7a1c2d18cf248a430773fab2332e05912d3e91c5
pranaykumar8 <pranayraos9@gmail.com>
2024-06-20 16:52:26 +0530

    bitwise

---
+++ bitwise.java
@@ -0,0 +1,11 @@
+class Bitwise
+{
+	public static void main(String[] args)
+	{
+	int a=20,b=16;
+		System.out.println("AND "+(a&b));
+		System.out.println("OR "+(a|b));
+		System.out.println("XOR "+(a^b));
+		System.out.println("Complement "+(~a));
+	}
+}
